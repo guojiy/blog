5dd04cb0061b8799f8cfa1f8de58b320393845a3
guo <guo@guodeMac-mini.local>
2020-01-01 08:56:32 +0800

    搭建开发环境

--- src/server/service/ApiService.ts
+++
@@ -1,9 +0,0 @@
-export default class ApiService{
-    safeRequest: any
-    constructor({ safeRequest }) {
-        this.safeRequest = safeRequest;
-    }
-    public getInfo(url: string, arg?: Object, callback?: Function): Promise<Object> {
-        return this.safeRequest.myFetch(url, arg, callback);
-    }
-}
